0c30d0670a34f97b2fdacd9cabe30252cf42ae20
Steepy <steepy@orange.fr>
2023-02-22 13:12:58 +0100

    updated client side

--- Client-Side/kubejs/server_scripts/script.js
+++ Client-Side/kubejs/server_scripts/script.js
@@ -35,13 +35,114 @@ event.shaped(
 	}
   )
 
+  event.remove({ id: 'tiab:time_in_a_bottle' })
+
+  event.shaped(
+	Item.of('tiab:time_in_a_bottle', 1), // arg 1: output
+	[ 
+	  'ABA', 
+	  'BCB', // arg 2: the shape (array of strings)
+	  'ABA'  
+	],
+	{
+	  A: 'minecraft:nether_star', 
+	  B: 'minecraft:netherite_block',  //arg 3: the mapping object
+	  C: 'cyclic:clock'
+ 
+	}
+  )
+
+  event.shaped(
+	Item.of('everlastingabilities:ability_totem', '{"everlastingabilities:abilityStoreStack":[{level:1,name:"everlastingabilities:bonemealer"}]}'),
+	[ 
+	  'ABA', 
+	  'BCB', // arg 2: the shape (array of strings)
+	  'ABA'  
+	],
+	{
+	  A: 'minecraft:bone_block', 
+	  B: 'mysticalagriculture:mystical_fertilizer',  //arg 3: the mapping object
+	  C: Item.of('mysticalagriculture:supremium_watering_can').ignoreNBT()
+ 
+	}
+  )
+
+  event.shaped(
+	Item.of('everlastingabilities:ability_totem', '{"everlastingabilities:abilityStoreStack":[{level:1,name:"everlastingabilities:magnetize"}]}'),
+	[ 
+	  'ABA', 
+	  'BCB', // arg 2: the shape (array of strings)
+	  'ABA'  
+	],
+	{
+	  A: 'create_sa:copper_magnet', 
+	  B: 'minecraft:lodestone',  //arg 3: the mapping object
+	  C: Item.of('everlastingabilities:ability_bottle').ignoreNBT()
+ 
+	}
+  )
+
+  event.shaped(
+	Item.of('everlastingabilities:ability_totem', '{"everlastingabilities:abilityStoreStack":[{level:1,name:"everlastingabilities:flight"}]}'),
+	[ 
+	  'ABE', 
+	  'DCD', // arg 2: the shape (array of strings)
+	  'FBG'  
+	],
+	{
+	  A: 'twilightforest:carminite_block', 
+	  B: Item.of('minecraft:elytra', '{Damage:0}'),  //arg 3: the mapping object
+	  C: Item.of('everlastingabilities:ability_bottle').ignoreNBT(),
+	  D: 'minecraft:phantom_spawn_egg',
+	  E: 'twilightforest:fiery_block',
+	  F: 'twilightforest:knightmetal_block',
+	  G: 'twilightforest:steeleaf_block',
+
+ 
+	}
+  )
+
+
+event.shaped(
+  Item.of('everlastingabilities:ability_totem', '{"everlastingabilities:abilityStoreStack":[{level:3,name:"everlastingabilities:saturation"}]}'),
+  [ 
+	'ABA', 
+	'BCB', // arg 2: the shape (array of strings)
+	'ADA'  
+  ],
+	{  A: 'minecraft:gold_block', 
+	B: 'mysticalagradditions:supremium_apple',  //arg 3: the mapping object
+	C: Item.of('botania:infinite_fruit').ignoreNBT(),
+	D: 'cyclic:lunchbox',
+	}
+
+  )
+  
+
+  event.custom({
+	type: 'tconstruct:melting',
+	inputs: [
+		{
+			tag: 'hem:rose_copper_ingot',
+			amount: 90,
+		},
+
+	],
+	result: {
+		fluid: 'tconstruct:molten_rose_gold',
+		amount: 90,
+	},
+	temperature: 760,
+	time: 35
+})
+
+
 
 
 
 
 
 })
-
 
 
 
